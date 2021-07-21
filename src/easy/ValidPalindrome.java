ff65dcb8383c59a30171fd833dff50fc110b74aa
ogamoga <cupkek228@gmail.com>
2021-07-21 20:25:38 +0300

    922, 125, 852, 933, 917

---
+++ src/easy/ValidPalindrome.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class ValidPalindrome {
+    public static boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            while ((i < s.length()) && (!Character.isLetter(s.charAt(i))) && (!Character.isDigit(s.charAt(i)))) {
+                i++;
+            }
+            while ((j >= 0) && (!Character.isLetter(s.charAt(j))) && (!Character.isDigit(s.charAt(j)))) {
+                j--;
+            }
+            if ((i >= s.length() || (j < 0))) {
+                return true;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
