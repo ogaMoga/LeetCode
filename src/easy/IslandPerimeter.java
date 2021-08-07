b19370f375c2fbea0b2b256c5a9571c9663d799e
ogamoga <cupkek228@gmail.com>
2021-08-07 16:59:19 +0300

    191, 463

---
+++ src/easy/IslandPerimeter.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    ans += 4;
+                    if (i != 0 && grid[i - 1][j] == 1) {
+                        ans -= 2;
+                    }
+                    if (j != 0 && grid[i][j - 1] == 1) {
+                        ans -= 2;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/easy/NumberOf1Bits.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class NumberOf1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            res++;
+        }
+        return res;
+    }
+}
